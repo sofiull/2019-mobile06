4e8c6f19236f16fedfe229a76a38b98d24219450
sofiull <sofiulfr@gmail.com>
2019-10-02 14:41:42 +0700

    Menyelesaikan pengiriman bundle dari activity main ke fragment About (menyelesaikan Bundle)

--- app/src/main/java/id/ac/polinema/idealbodyweight/fragments/AboutFragment.java
+++ app/src/main/java/id/ac/polinema/idealbodyweight/fragments/AboutFragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import id.ac.polinema.idealbodyweight.R;
 
@@ -19,12 +20,10 @@ import id.ac.polinema.idealbodyweight.R;
 public class AboutFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String ARG_NAME= "name";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private String name;
 
 
     public AboutFragment() {
@@ -40,11 +39,10 @@ public class AboutFragment extends Fragment {
      * @return A new instance of fragment AboutFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static AboutFragment newInstance(String param1, String param2) {
+    public static AboutFragment newInstance(String name) {
         AboutFragment fragment = new AboutFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_NAME, name);
         fragment.setArguments(args);
         return fragment;
     }
@@ -53,16 +51,17 @@ public class AboutFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            name = getArguments().getString(ARG_NAME);
         }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about,container,false);
+        TextView nameText = view.findViewById(R.id.text_name);
+        nameText.setText(name);
+        return view;
     }
 
 }
